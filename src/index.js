45b091a4ef9bda1623ea3a4c0870e329b27762dd
EdwinWalela <edwinwalela@gmail.com>
2022-01-23 10:42:20 +0300

    deregister peer

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const DB_URI = process.env.DB_URI;
 
+const Peer = require("./models/peer");
+
 
 mongoose.connect(DB_URI,{useNewUrlParser:true,useUnifiedTopology:true},()=>{
     console.log('DB connection successful');
@@ -17,10 +19,49 @@ app.use(cors());
 app.use(express.urlencoded({extended:true}))
 app.use(express.json());
 
-app.get('/',(req,res)=>{
+app.get('/peers',async (req,res)=>{
+    let peers = await Peer.find({},{_id:0});
+
     res.status(200).send({
-        docs:"https://documenter.getpostman.com/view/4654837/UVXesJR2"
+        peers
     })
+})
+
+app.post('/peers',async(req,res)=>{
+    let url = req.url
+
+    try{
+    await new Peer({
+        url:peer
+    }).save()
+
+    res.status(201).send({
+        msg:"peer registered"
+    })
+    return
+    }catch(err){
+        console.log(err)
+        res.status(500).send({
+            msg:"failed to register peer"
+        })
+    }
+})
+
+app.delete('/peers',async(req,res)=>{
+    let url = req.url
+
+    try{
+        await Peer.findOneAndDelete({url:url});
+        res.status(200).send({
+            msg:"deregistered peer"
+        })
+        return
+    }catch(err){
+        res.status(500).send({
+            msg:"failed to deregister peer"
+        })
+        return
+    }
 })
 
 app.listen(PORT,()=>{
